f95b4b2255a3c6d4ee0404db6bb927a836d50b0b
conner <ccmdehoop1991@gmail.com>
2026-01-15 22:46:05 +0100

    Refined routing logic to default to `/portfolio/`

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    base: "/portfolio/",
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        navigateFallback: '/portfolio/'
+      }
+    })
+  ],
+  base: "/portfolio/",
 })
